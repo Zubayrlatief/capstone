6ec15152d63b9efd65b24e87c8b08736863fa391
zubayrlatief <zubayrlatie007@gmail.com>
2024-08-28 23:23:12 +0200

    somechanges

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    
+ <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">projects <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">testimonials</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">contact me</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<LandingPage/>
+
 </template>
+
+<script>
+import LandingPage from './components/LandingPage.vue';
+  export default{
+    components:{
+      LandingPage
+    }
+  }
+</script>
 
 <style>
 
