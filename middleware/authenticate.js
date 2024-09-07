b332912fe7e4c49ba25a0eee8e0a3228b3e28a70
zubayrlatief <zubayrlatie007@gmail.com>
2024-09-07 13:04:17 +0200

    login

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -7,26 +7,55 @@ dotenv.config()
 
 const checkUser = async(req,res,next)=>{
     const {emailAdd,userPass} = req.body
-    console.log(emailAdd,userPass);
+    console.log("Email:", emailAdd, "Password:", userPass);
+
+    try {
+        const user = await loginUserDb(emailAdd);
+        if (!user) {
+            return res.status(400).send('User not found');
+        }
+
+        const hashedPassword = user.userPass;
+        console.log("Hashed Password:", hashedPassword);
+        
+        compare(userPass, hashedPassword, (err, result) => {
+            if (err) {
+                console.log("Error comparing passwords");
+            }
+        
+            if (result) {
+                const token = jwt.sign({ emailAdd: emailAdd }, process.env.SECRET_KEY, { expiresIn: "1h" });
+                console.log("Token:", token);
+                req.body.token = token;
+                next();
+            } else {
+                res.status(401).send('Incorrect password');
+            }
+        });
+    } catch (err) {
+        console.log("Error in checkUser middleware", error);
+        res.status(500).send('Internal Server Error');
+    }
+    };
     
-    let hashedPassword = (await loginUserDb(emailAdd)).userPass
+    // let hashedPassword = (await loginUserDb(emailAdd)).userPass
     // let hashedPassword = (await loginUserDb(emailAdd))
 
-    console.log(hashedPassword);
+//     console.log(hashedPassword);
 
-    compare(userPass, hashedPassword, (err,result)=> {
-        console.log(err);
+//     compare(userPass, hashedPassword, (err,result)=> {
+//         console.log(err);
         
-        if(result==true){
-            let token = jwt.sign({emailAdd:emailAdd},process.env.SECRET_KEY,{expiresIn:'1h'})
-            console.log(token);
-            req.body.token = token
-            next()
-            return
-        }
-        else{
-        res.send('Your password is incorrect')
-    }
-    })
-}
+//         if(result==true){
+//             let token = jwt.sign({emailAdd:emailAdd},process.env.SECRET_KEY,{expiresIn:'1h'})
+//             console.log(token);
+//             req.body.token = token
+//             next()
+//             return
+//         }
+//         else{
+//         res.send('Your password is incorrect')
+//     }
+//     })
+// }
 export {checkUser}
