f5278400b81ce5b51fc9943b35ba31c2f63d0fb0
zubayrlatief <zubayrlatie007@gmail.com>
2024-09-13 15:51:08 +0200

    code-indentation-2

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ import cartRouter from './routes/cartRouter.js';
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use(express.urlencoded({ extended: true }));
-
+const allowedOrigins = ['http://localhost:8080', 'https://capstone-965d2.web.app'];
 
 app.use(cors({
-  origin: 'http://localhost:8080', 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true 
 }));
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+// Routes
 app.use('/items', itemsRouter);
 app.use('/users', usersRouter);
 app.use('/api', cartRouter);
@@ -27,5 +34,3 @@ app.use('/api', cartRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
