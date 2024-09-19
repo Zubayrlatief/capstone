8aa5c461dbc8e276327d501817cff81d99b4b0a6
Zubayr-Latief <zubayrlatie007@gmail.com>
2024-09-19 12:10:12 +0200

    k

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ import cartRouter from './routes/cartRouter.js';
 const app = express();
 const port = process.env.PORT || 5001;
 
-const allowedOrigins = ['http://localhost:8080', 'https://capstone-965d2.web.app'];
+const allowedOrigins = ['http://localhost:8081', 'https://capstone-965d2.web.app'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
